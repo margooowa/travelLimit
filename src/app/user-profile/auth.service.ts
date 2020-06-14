6bbe47f00b9a66502a51602dfc8367bca605d458
Margarita <marharyta.kozub@infopulse.com>
2020-06-14 22:15:54 +0300

    ignore

--- src/app/user-profile/auth.service.ts
+++ src/app/user-profile/auth.service.ts
@@ -1,11 +1,9 @@
 import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
 import {User} from './user.model';
 import {AngularFireAuth} from '@angular/fire/auth';
-import {AngularFirestore, AngularFirestoreDocument} from '@angular/fire/firestore';
+import {AngularFirestore} from '@angular/fire/firestore';
 import {Router} from '@angular/router';
-import {auth} from 'firebase';
-import {switchMap} from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -26,42 +24,43 @@ export class AuthService {
         private afs: AngularFirestore,
         private router: Router
     ) {// Get the auth state, then fetch the Firestore user document or return null
-        this.user$ = this.afAuth.authState.pipe(
-            switchMap(user => {
-                // Logged in
-                if (user) {
-                    return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
-                } else {
-                    // Logged out
-                    return of(null);
-                }
-            })
-        )
+        // this.user$ = this.afAuth.authState.pipe(
+        //     switchMap(user => {
+        //         // Logged in
+        //         if (user) {
+        //             return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        //         } else {
+        //             // Logged out
+        //             return of(null);
+        //         }
+        //     })
+        // )
     }
 
-    async googleSignin() {
-        const provider = new auth.GoogleAuthProvider();
-        const credential = await this.afAuth.signInWithPopup(provider);
-        return this.updateUserData(credential.user);
-    }
+    // async googleSignin() {
+    //     const provider = new auth.GoogleAuthProvider();
+    //     const credential = await this.afAuth.signInWithPopup(provider);
+    //     return this.updateUserData(credential.user);
+    // }
 
-    private updateUserData(user) {
-        // Sets user data to firestore on login
-        const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
-
-        const data = {
-            uid: user.uid,
-            email: user.email,
-            displayName: user.displayName,
-            photoURL: user.photoURL
-        }
-
-        return userRef.set(data, {merge: true})
-
-    }
+    // private updateUserData(user) {
+    //     // Sets user data to firestore on login
+    //     const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+    //
+    //     const data = {
+    //         uid: user.uid,
+    //         email: user.email,
+    //         displayName: user.displayName,
+    //         photoURL: user.photoURL
+    //     }
+    //
+    //     return userRef.set(data, {merge: true})
+    //
+    // }
 
     async signOut() {
         await this.afAuth.signOut();
+        this.user.email = null;
         this.router.navigate(['/']);
     }
 
@@ -76,6 +75,8 @@ export class AuthService {
                 window.alert(error.message)
             })
     }
+
+
 
 
 }
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,16 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from './auth.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+    selector: 'app-user-profile',
+    templateUrl: './user-profile.component.html',
+    styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+    form: FormGroup;
+    public loginInvalid: boolean;
+    private formSubmitAttempt: boolean;
+    private returnUrl: string;
 
-  constructor(public auth: AuthService) { }
+    constructor(
+        private fb: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private auth: AuthService
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    async ngOnInit() {
+        this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/game';
 
+        this.form = this.fb.group({
+            username: ['', Validators.email],
+            password: ['', Validators.required]
+        });
+
+        // if (await this.authService.checkAuthenticated()) {
+        //   await this.router.navigate([this.returnUrl]);
+        // }
+    }
+
+    async onSubmit() {
+        this.loginInvalid = false;
+        this.formSubmitAttempt = false;
+        if (this.form.valid) {
+            try {
+                const username = this.form.get('username').value;
+                const password = this.form.get('password').value;
+                await this.auth.SignIn(username, password);
+            } catch (err) {
+                this.loginInvalid = true;
+            }
+        } else {
+            this.formSubmitAttempt = true;
+        }
+    }
 }
